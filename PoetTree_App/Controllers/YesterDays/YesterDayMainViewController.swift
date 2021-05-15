ebe3b7fa7eb0a2f82ec4262cb166c9a9c9c57422
김동환 <1033513@naver.com>
2021-05-15 14:37:00 +0900

    yesterdays cell writer name added

--- PoetTree_App/Controllers/YesterDays/YesterDayMainViewController.swift
+++ PoetTree_App/Controllers/YesterDays/YesterDayMainViewController.swift
@@ -99,15 +99,3 @@ extension YesterDayMainViewController: UITableViewDataSource{
         return cell
     }
 }
-
-extension UIImage {
-    func load(url: URL) {
-        DispatchQueue.global().async { [weak self] in
-            if let data = try? Data(contentsOf: url) {
-                if let image = UIImage(data: data) {
-                    self = image
-                }
-            }
-        }
-    }
-}
--- PoetTree_App/View/PostsCell.swift
+++ PoetTree_App/View/PostsCell.swift
@@ -13,12 +13,14 @@ class PostsCell: UITableViewCell {
     @IBOutlet weak var daysImage: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var likesLabel: UILabel!
+    @IBOutlet weak var writerLabel: UILabel!
     
     func updateCell(writing: WritingGet){
         
         daysImage.image = writing.image
         titleLabel.text = writing.title
         likesLabel.text = "❤️: \(writing.likes)"
+        writerLabel.text = "by. \(writing.userName)"
         
     }
     
