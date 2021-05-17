9c5a4da87ce32fffef654e466475ac72c8b5c0b2
김동환 <1033513@naver.com>
2021-05-18 01:52:02 +0900

    user writing put completed

--- PoetTree_App/Controllers/UserWriting/UserWritings/UserDetailWritingViewController.swift
+++ PoetTree_App/Controllers/UserWriting/UserWritings/UserDetailWritingViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 import Alamofire
-
+import SwiftyJSON
 //좋아요, 댓글, 수정
 
 class UserDetailWritingViewController: UIViewController {
@@ -17,17 +17,62 @@ class UserDetailWritingViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var userLabel: UILabel!
     @IBOutlet weak var contentLabel: UILabel!
-    
+    @IBOutlet weak var hashtagLabel: UILabel!
     
     var userWriting: UserWriting?
+    var hashtags: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setHashTag(id: userWriting?.id)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("vwacalled")
         setUI()
+        
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == K.SEGUE_ID.toUserWritingCorrection {
+            guard let vc = segue.destination as? UserWritingCorrectionViewController else {return}
+            
+            vc.userWriting = self.userWriting
+            vc.hashtags = self.hashtags
+            vc.changeUI = { writing in
+                
+                self.userWriting?.title = writing.title
+                self.userWriting?.content = writing.content
+                self.hashtags = "\(writing.hashtags[0]) \(writing.hashtags[1])"
+
+                self.hashtagLabel.text = self.hashtags
+                
+            }
+        }
+    }
+    
+    fileprivate func setHashTag(id: Int?){
+        
+        guard let id = id else {return}
+        
+        AF.request(K.API.WRITING_GET_POST + "\(id)", method: .get).responseJSON { [weak self] response in
+            
+            guard let self = self else {return}
+            
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                
+                if let hashtags = json["hashtags"].string {
+                    
+                    self.hashtags = hashtags
+                    self.hashtagLabel.text = hashtags
+                }
+            
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
             
         }
     }
@@ -37,6 +82,7 @@ class UserDetailWritingViewController: UIViewController {
         self.titleLabel.text = userWriting?.title
         self.contentLabel.text = userWriting?.content
         self.userLabel.text = userWriting?.name
+        self.hashtagLabel.text = hashtags
     }
     
     
@@ -50,6 +96,13 @@ class UserDetailWritingViewController: UIViewController {
             self.navigationController?.popViewController(animated: true)
             
         }
+    }
+    
+    @IBAction func likeBtnTapped(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func commentBtnTapped(_ sender: UIButton) {
     }
     
     
--- PoetTree_App/Controllers/UserWriting/UserWritings/UserWritingCorrectionViewController.swift
+++ PoetTree_App/Controllers/UserWriting/UserWritings/UserWritingCorrectionViewController.swift
@@ -6,23 +6,86 @@
 //
 
 import UIKit
+import Alamofire
 
 class UserWritingCorrectionViewController: UIViewController {
     
-    @IBOutlet weak var titleLabel: UITextField!
+    @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var userLabel: UILabel!
     @IBOutlet weak var contentTextView: UITextView!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var hashtag1TextField: UITextField!
+    @IBOutlet weak var hashtag2TextField: UITextField!
     
-    
+    var changeUI: ((Writing) -> Void)?
+    var userWriting: UserWriting?
+    var hashtags: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        hashtag1TextField.delegate = self
+        hashtag2TextField.delegate = self
+        setUI()
+    }
+    
+    fileprivate func setUI(){
+        
+        titleTextField.text = userWriting?.title
+        userLabel.text = userWriting?.name
+        contentTextView.text = userWriting?.content
+        imageView.image = userWriting?.image
+        
+        guard let hashtags = self.hashtags else {return}
+        
+        let hashtagsArr = hashtags.components(separatedBy: " ")
+        self.hashtag1TextField.text = hashtagsArr[0]
+        self.hashtag2TextField.text = hashtagsArr[1]
+        
     }
     
     
     @IBAction func completeBtnTapped(_ sender: UIButton) {
+        
+        guard let id = userWriting?.id,
+              let user = GoogleLogInViewController.user,
+              let title = titleTextField.text,
+              let content = contentTextView.text,
+              let hashtag1 = hashtag1TextField.text,
+              let hashtag2 = hashtag2TextField.text else {return}
+        
+        let parameters: [String: Any] = ["title" : title, "content" : content, "email" : user.profile.email, "hashtags" : "\(hashtag1) \(hashtag2)"]
+        
+        AF.request(K.API.WRITING_GET_POST+"\(id)", method: .put, parameters: parameters, encoding: JSONEncoding.default, interceptor: RequestInterceptor()).response{
+            response in
+            print(response)
+            let writing = Writing(title: title, content: content, email: user.profile.email, hashtags: [hashtag1, hashtag2])
+            self.changeUI?(writing)
+            self.navigationController?.popViewController(animated: true)
+        }
     }
     
+}
+
+extension UserWritingCorrectionViewController: UITextFieldDelegate {
     
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        guard let text = textField.text else {return false}
+        
+        let filteredText = text.filter{$0 != "#"}
+        
+        let finalText = "#\(filteredText)"
+        
+        if self.hashtag1TextField.isFirstResponder {
+            
+            self.hashtag1TextField.text = finalText
+        }
+        
+        if self.hashtag2TextField.isFirstResponder {
+            self.hashtag2TextField.text = finalText
+        }
+        
+        return true
+    }
 }
